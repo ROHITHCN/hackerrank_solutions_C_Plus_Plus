3a5cef089aae1d3dab9a34950f80c8aa148181f9
ROHITHCN <87167861+ROHITHCN@users.noreply.github.com>
2021-08-26 17:24:43 +0530

    Create Input_and_Output.cpp

---
+++ Input_and_Output.cpp
@@ -0,0 +1,16 @@
+// Objective
+// In this challenge, we practice reading input from stdin and printing output to stdout.
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+int a,b,c;
+cin>>a>>b>>c;
+cout<<a+b+c; 
+return 0;
+}
